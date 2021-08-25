d43ec348e91519106e782e3e8fdda71c672eb4df
Soncv <soncv97@gmail.com>
2021-08-25 16:04:12 +0700

    update hello and testà

--- test.java
+++ test.java
@@ -1,3 +1,9 @@
 public class test {
-    
-}
+    public static void main(String args[]){
+        a:    
+        for(int i = 0; i<= 5; i++) {      
+        c:    
+        for (int k = 0; k<=10; k++) {    
+        System.out.println(k);    
+    }
+}}}
