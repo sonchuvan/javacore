7272e24a272718bfbfe8c0ec10403dd13ad22648
Soncv <soncv97@gmail.com>
2021-08-27 17:18:23 +0700

    add file Armstrong and update test

---
+++ ArmstrongNumber.java
@@ -0,0 +1,25 @@
+import java.lang.Math;
+class ArmstrongNumber{
+    static void checkArmstrongNumber(int n){
+        int count = 0;
+        int temp = n; 
+        int temp2 = n;
+        do{
+            temp = temp/10;
+            count++;
+        }while(temp>0);
+        
+        int last;
+        double sum = 0;
+        do{
+            last = n%10;
+            sum = sum + Math.pow(last,count);
+            n = n/10;
+        }while(n > 0);
+        if(sum == temp2) System.out.print("is Armstrong number");
+        else System.out.print("is not Armstrong number");
+    }
+    public static void main(String args[]){
+        checkArmstrongNumber(3);
+    }
+}
